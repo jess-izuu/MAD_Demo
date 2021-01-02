844d2fd54e137bfc8d30ca4a57a847b278ed285c
jess-izuu <izumijessica@gmail.com>
2021-01-02 13:14:15 -0500

    walkthrough

--- MadDemo/ViewController.swift
+++ MadDemo/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
-
+    @IBAction func changeTextColor(_ sender: UIButton) {
+        textLabel.textColor = UIColor.purple
+    }
+    
+    @IBAction func changeViewColor(_ sender: UIButton) {
+        view.backgroundColor = UIColor.red
+    }
+    
+    @IBAction func changeTextString(_ sender: UIButton) {
+        textLabel.text = "GoodBye!"
+    }
 }
 
